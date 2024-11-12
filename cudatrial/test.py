3e580c150fec063257b62d902dbbced0867fee88
nadim kanazi <ngk2112@columbia.edu>
2024-11-12 00:28:48 -0500

    testing simultaneous calls

--- cudatrial/test.py
+++ cudatrial/test.py
@@ -2,22 +2,30 @@ import torch
 from torch.utils import cpp_extension
 
 # Check if CUDA is available
-print(torch.cuda.is_available())
+print(f'Cuda is available: {torch.cuda.is_available()}')
 
-# Load the shared library that contains the CUDA kernel
+
+# Load the shared library containing the CUDA kernel
 kernel_lib = cpp_extension.load(name='kernel', sources=['kernel.cu'], verbose=True)
 
 # Create a tensor on the GPU
-tensor = torch.zeros((), dtype=torch.float32, device='cuda:0')
+tensor = torch.zeros((3,3), dtype=torch.float32, device='cuda:0')
 
 # Print the original value
-print("Original value:", tensor.item())
+print("Original value:", tensor)
 
-# Call the CUDA kernel
-# This will directly pass the tensor to the kernel function
-kernel_lib.add_one_at_address(tensor)
+# Print tensor address before call
+print(f"Memory address before kernel call: {tensor.data_ptr()}")
+
+
+# Call the CUDA kernel multiple times in a loop
+for _ in range(3):
+    kernel_lib.add_one_at_address(tensor)
+    tensor[0,0] += 1
 
 # Check the result after modifying the tensor's value via the kernel
-print("Modified value:", tensor.item())
+print("Modified value:", tensor)
 
+# Print tensor address after call:
+print(f"Memory address after kernel call: {tensor.data_ptr()}")
 
